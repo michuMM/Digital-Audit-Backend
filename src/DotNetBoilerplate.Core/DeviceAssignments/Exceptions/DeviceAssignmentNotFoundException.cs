b1eda17a0cdf6ccb571d15b617dd196b73d204ec
Tempiusz <zanczakjakub@gmail.com>
2024-07-24 23:54:04 +0200

    Corrections

--- src/DotNetBoilerplate.Core/DeviceAssignments/Exceptions/DeviceAssignmentNotFoundException.cs
+++ src/DotNetBoilerplate.Core/DeviceAssignments/Exceptions/DeviceAssignmentNotFoundException.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DotNetBoilerplate.Shared.Abstractions.Exceptions;
 
-namespace DotNetBoilerplate.Core.DeviceAssignments.Exceptions
+namespace DotNetBoilerplate.Core.DeviceAssignments.Exceptions;
+
+public sealed class DeviceAssignmentNotFoundException : CustomException
 {
-    internal class DeviceAssignmentNotFoundException
+    public Guid DeviceAssignmentId { get; }
+
+    public DeviceAssignmentNotFoundException(Guid deviceAssignmentId)
+        : base($"Device assignment with Id {deviceAssignmentId} not found.")
     {
+        DeviceAssignmentId = deviceAssignmentId;
     }
 }
