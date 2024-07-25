82ba3503691437d36bcdefb88bcb06c65f98cd68
Michał Mroczka <michal.mroczka03@gmail.com>
2024-07-25 16:26:17 +0200

    Added DeleteDeviceCategory feature.

---
+++ src/DotNetBoilerplate.Api/DeviceCategories/DeleteDeviceCategoryEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using DotNetBoilerplate.Application.DeviceCategories.Delete;
+using DotNetBoilerplate.Shared.Abstractions.Commands;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using static DotNetBoilerplate.Core.DeviceCategories.DeviceCategory;
+
+namespace DotNetBoilerplate.Api.DeviceCategories;
+
+internal sealed class DeleteDeviceCategoryEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("{id:guid}", Handle)
+            .RequireAuthorization()
+            .WithSummary("Delete device category by ID");
+    }
+
+    private static async Task<Ok<Response>> Handle(
+        Guid id,
+        [FromServices] ICommandDispatcher commandDispatcher,
+        CancellationToken ct
+    )
+    {
+        var command = new DeleteDeviceCategoryCommand(id);
+
+        await commandDispatcher.DispatchAsync<DeleteDeviceCategoryCommand>(command, ct);
+
+        return TypedResults.Ok(new Response());
+    }
+
+    internal sealed record Response();
+}
---
+++ src/DotNetBoilerplate.Application/DeviceCategories/Delete/DeleteDeviceCategoryCommand.cs
@@ -0,0 +1,5 @@
+﻿using DotNetBoilerplate.Shared.Abstractions.Commands;
+
+namespace DotNetBoilerplate.Application.DeviceCategories.Delete;
+
+public sealed record DeleteDeviceCategoryCommand(Guid id) : ICommand<Guid>;
