84bbe11cec5ce053b79bed0f0adae73b38304138
Michał Mroczka <michal.mroczka03@gmail.com>
2024-08-07 15:38:42 +0200

    Deleted semicolon again

--- src/DotNetBoilerplate.Api/DeviceAssignments/DeviceAssignmentsEndpoints.cs
+++ src/DotNetBoilerplate.Api/DeviceAssignments/DeviceAssignmentsEndpoints.cs
@@ -17,7 +17,7 @@ internal static class DeviceAssignmentsEndpoints
             .MapEndpoint<GetAllDeviceAssignmentsEndpoint>()
             .MapEndpoint<GetDeviceAssignmentByIdEndpoint>()                        
             .MapEndpoint<ReturnDeviceByIdEndpoint>()
-            .MapEndpoint<GetAllDeviceAssignmentsByOrganizationIdEndpoint>();
+            .MapEndpoint<GetAllDeviceAssignmentsByOrganizationIdEndpoint>()
             .MapEndpoint<ModifyDeviceAssignmentEndpoint>();
     }
 }
